a8361caa49c995e34dac1cea3e1ccc492d544466
Kirhhoff <1070280566@qq.com>
2019-03-04 00:02:20 +0800

    -编写了JWTManager的测试类，测试了编码和解码结果的一致性

---
+++ src/test/java/com/scut/se/sehubbackend/JWTManagerTest.java
@@ -0,0 +1,44 @@
+package com.scut.se.sehubbackend;
+
+
+import com.scut.se.sehubbackend.Domain.User;
+import com.scut.se.sehubbackend.JWT.JWTManager;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.lang.JoseException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertTrue;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SeHubBackendApplication.class)
+public class JWTManagerTest {
+
+    @Autowired
+    JWTManager jwtManager;
+
+    @Mock
+    User user;
+
+    @Before
+    public void before(){
+        when(user.getStudentNO()).thenReturn("201730683314");
+    }
+
+    @Test
+    public void testEncodeAndDecode() throws JoseException, MalformedClaimException, InvalidJwtException {
+        String jwt=jwtManager.encode(user);//对user编码
+        User edUser=jwtManager.decode(jwt);//再解码
+        Assert.assertEquals(user.getStudentNO(),edUser.getStudentNO());//两者的studentNO应该一样
+    }
+}
