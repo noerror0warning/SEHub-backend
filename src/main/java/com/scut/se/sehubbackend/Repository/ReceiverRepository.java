6b417cda8501a626008bef35db774b91c0f771ca
Kirhhoff <1070280566@qq.com>
2019-04-13 21:54:28 +0800

    移除冗余表

--- src/main/java/com/scut/se/sehubbackend/Repository/ReceiverRepository.java
+++
@@ -1,6 +0,0 @@
-package com.scut.se.sehubbackend.Repository;
-
-//@Repository
-//public interface ReceiverRepository extends JpaRepository<Receiver,Long> {
-//
-//}
--- src/main/java/com/scut/se/sehubbackend/Repository/UserExtraRepository.java
+++
@@ -1,6 +0,0 @@
-package com.scut.se.sehubbackend.Repository;
-
-//@Repository
-//public interface UserExtraRepository extends JpaRepository<UserExtra,Long> {
-//
-//}
